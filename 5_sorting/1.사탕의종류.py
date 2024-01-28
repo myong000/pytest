f6439dd79f2b3a82d4ddab368af412c0ea7da1a0
myong000 <leemoung1@naver.com>
2024-01-28 23:50:49 +0900

    aa

---
+++ 5_sorting/1.사탕의종류.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    answer = 0
+    answer = len(nums)/2
+
+    list=(sorted(nums))
+    bb=set(list)
+    aa=len(bb)
+
+    if answer > aa:
+        answer = aa
+            
+    return answer
+    
+                       
+print(solution([2, 1, 1, 3, 2, 3, 1, 2]))
+print(solution([1, 3, 5, 7, 2, 3, 7, 5, 3, 2, 5, 7, 9, 12]))
+print(solution([5, 5, 5, 5, 5, 5]))
+print(solution([12, 23, 11, 3, 5, 23, 23, 23, 23, 23, 23, 23]))
+print(solution([100, 200, 300, 400, 500, 600, 700, 800]))
